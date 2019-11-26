5c97be70fefebca42517c356cde89a8ac11ca5c5
Davi <30638614+BrandyLover@users.noreply.github.com>
2019-11-26 13:34:50 -0300

    revert teste

--- unit1.pas
+++ unit1.pas
@@ -5,19 +5,10 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls, ExtCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
 
 type
-
-  { TForm1 }
-
   TForm1 = class(TForm)
-    PageControl1: TPageControl;
-    TabSheet1: TTabSheet;
-    TabSheet2: TTabSheet;
-    TabSheet3: TTabSheet;
-    TabSheet4: TTabSheet;
-
   private
 
   public
@@ -30,11 +21,6 @@ var
 implementation
 
 {$R *.lfm}
-
-{ TForm1 }
-
-
-
 
 end.
 
